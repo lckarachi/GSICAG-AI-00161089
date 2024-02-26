0797f6f017612c2e81a06e7c40694ded3768c6d4
lckarachi <56872754+lckarachi@users.noreply.github.com>
2024-02-26 02:31:47 -0800

    Class #1
    in class assignment

---
+++ main.ts
@@ -0,0 +1,6 @@
+console.log("Hello, World!");
+// program from tuttor below
+const Character = 'customer';
+console.log(Character);
+const inputs = document.querySelectorAll('input');
+console.log(inputs);
